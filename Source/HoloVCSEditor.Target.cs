7c11f5f313af949fdbf9bcd99a249c840d7adbfe
Seth Robinson <seth@rtsoft.com>
2021-08-16 18:29:18 +0900

    More of the initial commit

---
+++ Source/HoloVCSEditor.Target.cs
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class HoloVCSEditorTarget : TargetRules
+{
+	public HoloVCSEditorTarget( TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+		ExtraModuleNames.AddRange( new string[] { "HoloVCS" } );
+		
+	}
+}
